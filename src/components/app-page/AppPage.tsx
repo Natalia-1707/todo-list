5afadf96a35e137eb3e0f07dc5c8a1240180d243
natalia-1707 <nataliatsepoukhowa@gmail.com>
2026-09-09 20:46:47 +0300

    feat: make visual structure of app-page

--- src/components/app-page/AppPage.tsx
+++ src/components/app-page/AppPage.tsx
@@ -1,5 +1,26 @@
+import styles from './app-page.module.css';
+
 function AppPage() {
-  return <div>In Process</div>;
+  const buttons = [
+    { id: 'tasks', title: 'Мои задачи' },
+    { id: 'diary', title: 'Дневник' },
+    { id: 'birthdays', title: 'Дни рождения' },
+    { id: 'books', title: 'Мои книги и фильмы' },
+    { id: 'motivation', title: 'Мотивашки' },
+  ];
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <h2>Привет, ИМЯ!</h2>
+        <p>00:00:00</p>
+      </div>
+      <div className={styles.buttons}>
+        {buttons.map((button) => (
+          <button key={button.id}>{button.title}</button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default AppPage;
