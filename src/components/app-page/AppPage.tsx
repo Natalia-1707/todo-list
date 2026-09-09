f15b00a86bf91be4c55721a512e3a6b868e2abe9
natalia-1707 <nataliatsepoukhowa@gmail.com>
2026-09-09 22:56:31 +0300

    style: update app-page design

--- src/components/app-page/AppPage.tsx
+++ src/components/app-page/AppPage.tsx
@@ -1,23 +1,31 @@
 import styles from './app-page.module.css';
+import tasksIcon from '../../assets/images/tasks.svg';
+import diaryIcon from '../../assets/images/diary.svg';
+import birthdayIcon from '../../assets/images/birthdays.svg';
+import booksIcon from '../../assets/images/movies.svg';
+import motivationIcon from '../../assets/images/motivation.svg';
 
 function AppPage() {
   const buttons = [
-    { id: 'tasks', title: 'Мои задачи', icon: '✅' },
-    { id: 'diary', title: 'Дневник', icon: '📖' },
-    { id: 'birthdays', title: 'Дни рождения', icon: '🎈' },
-    { id: 'books', title: 'Мои книги и фильмы', icon: '🎬' },
-    { id: 'motivation', title: 'Мотивашки', icon: '⭐' },
+    { id: 'tasks', title: 'Мои задачи', icon: tasksIcon },
+    { id: 'diary', title: 'Дневник', icon: diaryIcon },
+    { id: 'birthdays', title: 'Дни рождения', icon: birthdayIcon },
+    { id: 'books', title: 'Мои книги и фильмы', icon: booksIcon },
+    { id: 'motivation', title: 'Мотивашки', icon: motivationIcon },
   ];
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
-        <h2>Привет, ИМЯ!</h2>
+        <div className={styles.greeting}>
+          <h2>Привет, ИМЯ!</h2>
+          <span>Давай сделаем этот день продуктивным!</span>
+        </div>
         <p>00:00:00</p>
       </div>
       <div className={styles.buttons}>
         {buttons.map((button) => (
           <button key={button.id}>
-            <span className={styles.icon}>{button.icon}</span>
+            <img className={styles.icon} src={button.icon} alt="" />
             <span>{button.title}</span>
           </button>
         ))}
